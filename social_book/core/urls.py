b4bc47c2161dcaac5e021b00629e64a9de13d616
gliu <gliu25@students.ubc.ca>
2024-01-21 10:08:53 -0800

    ff

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name=index)
+     path('', views.index, name= 'index'),
+     path('index.html', views.index, name ='index'),
+     path('out-team.html', views.our_team, name = 'out-team'),
+     path('profile-page.html', views.profile_page, name = 'profile-page'),
+     path('search.html', views.search, name = 'search')
+
 ]
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+# index html is main page, put bill's main page as index.html in templates
+def index(request):
+    return render(request, 'index.html')
+
+def our_team(request):
+    return render(request, 'out-team.html')
+
+def profile_page(request):
+    return render(request, 'profile-page.html')
+
+def search(request):
+    return render(request, 'search.html')
+            
